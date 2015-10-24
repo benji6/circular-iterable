18475843aa1e4bf709b5dbe1a2e2def2708e5a30
benji6 <benhall22@hotmail.co.uk>
2015-10-24 11:52:16 +0100

    much nicer implementation

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ export default (...args) => {
   const {length} = args;
   const circularData = i => args[(i % length + length) % length];
   circularData[Symbol.iterator] = function* () {
-    let index = 0;
-    while (true) {
-      yield args[index];
-      index = index === length - 1 ? 0 : index + 1;
-    }
+    while (true) yield* args[Symbol.iterator]();
   };
   circularData.toArray = () => [...args];
   return circularData;
